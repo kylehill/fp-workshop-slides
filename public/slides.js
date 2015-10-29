456dbaa68204272c36e5e05a6c00623fb4860412
Kyle Hill <kylehill@gmail.com>
2015-10-28 20:30:56 -0400

    Revamp some slides for iteration 2

--- public/slides.js
+++ public/slides.js
@@ -20,47 +20,45 @@ var slides = [
     ]
   },
   {
-    title: "Functional programming is...",
-    ul: [
-      "One of several <em>patterns</em> involving",
-      "a <em>generic</em>, <em>independent</em> iterator function",
-      "executed on <em>each member</em> of a collection (read: array) in order",
-      "which is <em>non-mutative</em>",
-      "that <em>returns a value</em>",
-      "which is pattern-specifically <em>synthesized</em> into a single, returned value",
-      "(and also there's forEach which only does, like, some of that)"
-    ]
-  },
-  {
     title: "By example",
     code: [
-      "// An 'iterator' is a function that accepts a value", 
-      "// and returns a value",
       "var iterator = function(i) {",
       "  return i * 2 ",
       "}",
       "",
-      "// An array is, well, an array",
-      "var array = [ 10, 20, 30, 40 ]"
-    ]
-  },
-  {
-    title: "By example (cont'd)",
-    code: [
-      "/* The pattern is the specific higher-order operation",
-      "which we are performing",
+      "var array = [ 10, 20, 30, 40 ]",
       "",
-      "A pattern is a function that accepts both",
-      "an array and an iterator",
-      "",
-      "Patterns are reusable and generic -- you'll get different",
-      "results by changing either the array or the iterator",
-      "*/",
-      "pattern(array, iterator)"
+      "var result = pattern(array, iterator)",
+      "// or",
+      "var result = array.pattern(iterator)"
     ]
   },
   {
     content: "<h2>This requires <em>passing a function as a parameter</em> into another function</h2>"
+  },
+  {
+    code: [
+      "$(document).on('ready', function(){",
+      "  // do some stuff",
+      "})",
+      "",
+      "$('.green-button').on('click', function(evt){",
+      "  // do other stuff",
+      "})"
+    ]
+  },
+  {
+    code: [
+      "var startApplication = function(){",
+      "  // do some stuff",
+      "}",
+      "$(document).on('ready', startApplication)",
+      "",
+      "var clickGreenButton = function(evt){",
+      "  // do other stuff",
+      "}",
+      "$('.green-button').on('click', clickGreenButton)"
+    ]
   },
   {
     title: "FP foundational patterns",
@@ -73,25 +71,31 @@ var slides = [
   {
     title: "Foundational patterns - <em>map</em>",
     code: [
-      "[5, 10, 15].map(function(item){",
+      "var arrayOfNumbers = [5, 10, 15, 20, 25]",
+      "",
+      "arrayOfNumbers.map(function(item){",
       "  return item * 2",
-      "}) // [10, 20, 30]"
+      "}) // [10, 20, 30, 40 , 50]"
     ]
   },
   {
     title: "Foundational patterns -- <em>filter</em>",
     code: [
-      "[5, 10, 15].filter(function(item){",
-      "  return (item % 2 !== 0)",
-      "}) // [5, 15]"
+      "var arrayOfNumbers = [5, 10, 15, 20, 25]",
+      "",
+      "arrayOfNumbers.filter(function(item){",
+      "  return (item > 16)",
+      "}) // [20, 25]"
     ]
   },
   {
     title: "Foundational patterns -- <em>reduce</em>",
     code: [
-      "[5, 10, 15].reduce(function(memory, item){",
+      "var arrayOfNumbers = [5, 10, 15, 20, 25]",
+      "",
+      "arrayOfNumbers.reduce(function(memory, item){",
       "  return memory + item",
-      "}, 0) // 30"
+      "}, 0) // 75"
     ]
   }
 ]
